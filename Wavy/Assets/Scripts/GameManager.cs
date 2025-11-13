f7d5373510148ba8c606aa9ca9c2810e0eeef75a
Kaden Nielsen <tadernielsen@gmail.com>
2025-11-12 21:46:52 -0700

    Created GameManager Singleton

--- Wavy/Assets/Scripts/GameManager.cs
+++ Wavy/Assets/Scripts/GameManager.cs
@@ -4,15 +4,18 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public static GameManager Instance { get; private set; }
 
-    // Update is called once per frame
-    void Update()
+    void Awake()
     {
-        
+        if (Instance != null && Instance != this)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
     }
 }
