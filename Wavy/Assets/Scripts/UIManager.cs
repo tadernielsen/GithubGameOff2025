f027edc5eb08aeb18a62c4cfab13036339f50eb9
Kaden Nielsen <tadernielsen@gmail.com>
2025-11-12 21:06:15 -0700

    Added UI Manager

---
+++ Wavy/Assets/Scripts/UIManager.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class UIManager : MonoBehaviour
+{
+    public GameObject dropsBar;
+    public GameObject healthBar;
+    public GameObject itemBar;
+
+    private TextMeshProUGUI dropsValue;
+    private TextMeshProUGUI parValue;
+    private TextMeshProUGUI healthValue;
+    private TextMeshProUGUI objectName;
+
+    void Start()
+    {
+        dropsValue = dropsBar.transform.Find("Drops Number").GetComponent<TextMeshProUGUI>();
+        parValue = dropsBar.transform.Find("Par Number").GetComponent<TextMeshProUGUI>();
+        healthValue = healthBar.transform.Find("Health Number").GetComponent<TextMeshProUGUI>();
+        objectName = itemBar.transform.Find("Item").GetComponent<TextMeshProUGUI>();
+
+        test();
+    }
+
+    public void UpdateDrops(int drops)
+    {
+        dropsValue.text = drops.ToString();
+    }
+
+    public void UpdatePar(int par)
+    {
+        parValue.text = par.ToString();
+    }
+
+    public void UpdateHealth(int health)
+    {
+        healthValue.text = health.ToString();
+    }
+
+    public void UpdateObject(string itemName)
+    {
+        objectName.text = itemName;
+    }
+
+    private void test()
+    {
+        UpdateDrops(Random.Range(0, 100));
+        UpdatePar(Random.Range(0, 100));
+        UpdateHealth(Random.Range(0, 100));
+        UpdateObject("Test Item");
+    }
+}
